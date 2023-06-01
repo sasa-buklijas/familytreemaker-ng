51b6a361a402007d415c32a60a03c230016337f9
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-06-01 20:25:09 +0200

    basic parsing

--- familytreemake-ng.py
+++ familytreemake-ng.py
@@ -8,15 +8,47 @@ __version__ = "3.0"
 
 import argparse
 
+class FTError(Exception):
+    """A base class for FTError exceptions."""
+    
+class ParsingError(FTError):
+	"""A custom exception class for parsing input file."""
+
+
 class Family:
 	"""Represents the whole family.
 	"""
-	
+
 	def __init__(self, input_file: str):
 		# parsing input_file
+		# with more rigid format of input family file it is easier to parse it
 		with open(input_file, "r") as f:
 			for line_number, line in enumerate(f):
 				print(line_number, line, end='')
+
+				line = line.rstrip()
+				# maybe I will have same in \t later, then I can make it as function
+				# separated because it is not important
+				if line == '':  # after rstrip() \n is ''
+					print('    # SKIP/IGNORE EMPTY LINE')
+					continue
+				elif line[0] == '#':  # if it is comment, just skip it
+					print('    # SKIP/IGNORE COMMENT')
+					continue
+
+				if line[0] == '\t':
+					print('    # THIS IS CHILD')
+				elif len(line) >= 6:# (id=x)
+					print('    # THIS IS PERSON IN UNION')
+				else:
+					raise ParsingError(f'Do not know how to parse line number {line_number}::{line}')
+
+class Person:
+	"""Represents the person.
+	"""
+
+	def __init__(self, line_to_parse: str):
+		pass
 
 
 def main():
