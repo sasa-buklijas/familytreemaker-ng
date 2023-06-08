f06bc7fa51ba5c40d91b7655b541b4e05195750d
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-06-08 21:31:39 +0200

    parsing household

--- familytreemake_ng.py
+++ familytreemake_ng.py
@@ -7,9 +7,10 @@ __license__ = "GPL"
 __version__ = "3.0"
 
 import argparse
-from pathlib import Path
+#from pathlib import Path
 import re
 from dataclasses import dataclass
+from enum import Flag, auto
 
 class FTError(Exception):
     """A base class for FTError exceptions."""
@@ -47,16 +48,37 @@ class Family:
 	"""
 
 	def __init__(self, input_file: str):
-		self.__persons = dict()	# every person in family
-		self.__parse_input_file(input_file)
+		# TODO: make public accessors for _persons
+		self._persons = dict()	# every person in family
+		self._parse_input_file(input_file)
 		
-	def __parse_input_file(self, input_file: str):
+	def _parse_input_file(self, input_file: str):
 		"""parsing input_file
 			with more rigid format of input family file it is easier to parse it
 		"""
-		#def __parse_person():
 
+		class State():
+			'''When first first member of household is found,
+				second member of household must also be present,
+				otherwise exception is raised'''
 
+			class Line(Flag):
+				'''Representing line state, what is line'''
+				EMPTY = auto()
+				COMMENT = auto()
+				PERSON1 = auto()	# first member of household
+				PERSON2 = auto()	# second member of household
+				CHILD = auto()
+		
+			def __init__(self):
+				self.__first_member_of_household: bool = True
+
+			def validate_file(x: Line):
+				pass
+
+		#RODO: remove if stupid idea
+		#second_partner_is_next: bool = False
+		household = None
 		with open(input_file, "r") as f:
 			for line_number, line in enumerate(f, start=1):
 				print(line_number, line, end='')
@@ -65,20 +87,22 @@ class Family:
 				# maybe I will have same in \t later, then I can make it as function
 				# separated because it is not important
 				if line == '':  # after rstrip() \n is ''
-					print('    # SKIP/IGNORE EMPTY LINE')
+					#print('    # SKIP/IGNORE EMPTY LINE')
 					continue
 				elif line[0] == '#':  # if it is comment, just skip it
-					print('    # SKIP/IGNORE COMMENT')
+					#print('    # SKIP/IGNORE COMMENT')
 					continue
 
 				if line[0] == '\t':
 					print('    # THIS IS CHILD')
+					# TODO: will see how
 					# parse child
 					# child must start with name can not be mentioned first time
 				elif len(line) >= 6:# (id=x)
 					print('    # THIS IS PERSON IN UNION')
-
-					# pocni prvo samo s imenom i id
+					#FIXME: delete if not needed
+					#line_state = Line.PERSON
+					
 					regex = r'''
 						# start with main_name, what is first and second name together
 							# [\w ]+?, ? is there so that + is non-greedy
@@ -125,55 +149,90 @@ class Family:
 						exist: Person = self.__is_person_already_in_family(match.group('id'))
 						if exist:
 							raise DuplicatedPersonIDError(line_number, exist)
-
 						else:
 							new_person = Person(match.group('main_name'),
-											match.group('gender'),
-											match.group('id'),
-											match.group('birth_day'),
-											line_number)
-							self.__persons[match.group('id')] = new_person
-						# how to do if there are optional
+												match.group('gender'),
+												match.group('id'),
+												match.group('birth_day'),
+												line_number,
+												match.group('death_day'),	
+												match.group('maiden_name'),)
+							#TODO: do it alike property self.add_person(new_person), maybe not best idea
+							self._persons[match.group('id')] = new_person
 
-						print(f'	GROUP: {match.groupdict()}')
+							if not(household):	# first time
+								#TODO: not happy with this design, 
+								# thing that everything should be handled by Family class/object
+								household: Household = Household(new_person)
+							else:
+								household.add_householder(new_person)
+								# add household to family ???
+								household = None
+
+
+							
+
+
+						#second_partner_is_next = True
+
+						#print(f'	GROUP: {match.groupdict()}')
 
 					if line[0] == '(':
 						raise ParsingError(line_number, line)
 						# this is just id, person was defined before
 						# we just need to find id and make connection
 
-					if line[0] in (' ', '\t'):
+					if line[0] in (' ', '\t'):	# parsing children's
 						raise ParsingError(line_number, line)
 		
 				else:
 					raise ParsingError(line_number, line)
+			
+			if household:
+				raise ValueError(f'Household {household}, not completed, missing second person.')
 		
 		return True
 	
 	def __is_person_already_in_family(self, id: str):
 		'''Return TRUE if id/person is already in family'''
-		if id in self.__persons:
-			return self.__persons[id]
+
+		if id in self._persons:
+			return self._persons[id]
 		return False
-
-		# retunr self.__persons.get(self.__persons[id], False)
-
+	
 
 @dataclass(frozen=True)	# frozen=True means it can not be changed once it is created
 class Person:
 	"""Represents the person.
 	"""
-	main_name : str
-	gender : str
-	id : str
-	birth_day : str 
+	main_name: str
+	gender: str
+	id: str
+	birth_day: str 
 	first_seen_in_line: int	  # for debug is ti is seen
-	death_day : str = None
-	maiden_name : str = None
+	death_day: str = None
+	maiden_name: str = None
 	
+@dataclass()
+class Household:
+	'''Represent union of two people,
+		with or without children's'''
+	person1: Person		# first person is just first parsed person
+	person2: Person = None
+	# currently both persons can be same gender, can be changed in future
 
+	def add_householder(self, p: Person):
+		if self.is_full():
+			ValueError(f'Person {p}, is 3rd person in household. Only 2 persons allowed.')
+		self.person2 = p
 
-
+	# FIXME: currently not needed, keep it if you need it in future
+	#def is_empty(self):
+	#	return any(self.person1, self.person2)
+	
+	def is_full(self):
+		'''Household is full, if there are both persons'''
+		return all((self.person1, self.person2))
 
 def main():
 	# Parse command line options
@@ -186,6 +245,8 @@ def main():
 
 	# Create the family
 	family = Family(args.input_file)
+
+	print('\nfamily._persons:\n', family._persons)
 
 if __name__ == '__main__':
 	main()
--- test_.py
+++ test_.py
@@ -17,15 +17,10 @@ from familytreemake_ng import ParsingError, DuplicatedPersonIDError
         {'f': '\t\t'},
         {'f': '\n\t'},
         # person
-        {'f': 'x(M, id=1, birth_day=1,)'},
-        {'f': 'NAME_SURNAME (F,id=2,  birth_day=1,)'},
-        {'f': 'NAME SURNAME (F,   id=2,  birth_day=1,)'}, 
+        {'f': 'N_S (F,id=1,  birth_day=1,)\nNAME SURNAME (F,   id=2,  birth_day=1,)'},
         # spaces allowed after ), because it  is rstrip() 
-        {'f': 'm(M, id=1, birth_day=1954,)        '},
-        # ',' is optional in birth_day
-        {'f': 'm(M,   id=1,   birth_day=1954,   )'},
-        {'f': 'f(F, id=1, birth_day=1, death_day=9, maiden_name=mn,)'},
-
+        {'f': 
+        'm(M,id=1,birth_day=1954,) \nf(F,id=2,birth_day=1,death_day=9,maiden_name=m,)'},
     ],
     indirect=['tmp_files'],
 )
@@ -56,12 +51,25 @@ def test_is_person_line_invalid(tmp_files):
     ],
     indirect=['tmp_files'],
 )
-# add expected error messages
 def test_is_id_duplicated(tmp_files):
     with pytest.raises(DuplicatedPersonIDError) as e:    #DuplicatedPersonIDError
         t.Family(tmp_files / 'f')
         
-    print('----MY', str(e.value))
     part_of_expected_error_text = \
     'Person from line=2, have same id(already defined) as person from line=1--->Person'
     assert str(e.value).startswith(part_of_expected_error_text)
+
+# missing second person in household
+@pytest.mark.parametrize('tmp_files', [
+        {'f': 'a(M, id=1, birth_day=1,)'},
+    ],
+    indirect=['tmp_files'],
+)
+def test_is_household_completed(tmp_files):
+    with pytest.raises(ValueError) as e:    #DuplicatedPersonIDError
+        t.Family(tmp_files / 'f')
+        
+    start_with_error_text = 'Household '
+    ends_with_error_text = '), person2=None), not completed, missing second person.'
+    assert str(e.value).startswith(start_with_error_text)
+    assert str(e.value).endswith(ends_with_error_text)
